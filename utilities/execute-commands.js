d8cf49a636ba22bdfff8035a55f073fb31b92f3f
reallysxm <reallysxm@gmail.com>
2025-04-08 00:54:54 +0530

    Update 1.1

--- utilities/execute-commands.js
+++ utilities/execute-commands.js
@@ -1,3 +1,5 @@
+import path from "path";
+import loadJson from "../utilities/load-json.js";
 import { commands } from "../handlers/commands-handler.js";
 /**
  * Executes a command by its name.
@@ -7,10 +9,23 @@ import { commands } from "../handlers/commands-handler.js";
  * @param message - The Discord message object triggering the command.
  * @param args - Additional arguments to pass to the command.
  */
-export default function executeCommand(client, commandName, message, ...args) {
+export default async function executeCommand(client, commandName, message, ...args) {
     const command = commands.get(commandName);
     if (command) {
-        return command.execute(client, message, ...args);
+        try {
+            const configFilePath = "../config.json";
+            const { anonymousCommands } = await loadJson(path.resolve(import.meta.dirname, configFilePath), new URL(import.meta.url));
+            if (!anonymousCommands)
+                return command.execute(client, message, ...args);
+            if (message.deletable) {
+                command.execute(client, message, ...args);
+                await new Promise((resolve) => setTimeout(resolve, 1));
+                await message.delete();
+            }
+        }
+        catch (error) {
+            console.log("Failed to load the config file or anonymousCommands is missing:", error);
+        }
     }
     else
         return;

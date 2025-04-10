007f438e3e7225a539f8f38870887cfae20c3a54
reallysxm <reallysxm@gmail.com>
2025-04-10 23:32:30 +0530

    Fixed load-json.ts

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import path from "path";
 import loadJson from "./utilities/load-json.js";
-import eventHandler from "./handlers/events-handler.js";
-import commandHandler from "./handlers/commands-handler.js";
-import { Client } from "discord.js-selfbot-v13";
-const client = new Client();
+import { Client as DiscordClient } from "discord.js-selfbot-v13";
+const discordClient = new DiscordClient();
 const configFilePath = path.resolve(import.meta.dirname, "config.json"); //Add the relative path to your config file here
-try {
-    const { token } = await loadJson(configFilePath, new URL(import.meta.url));
-    await eventHandler(client);
-    await commandHandler(client);
-    client.login(token);
-}
-catch (error) {
-    console.error("Failed to load the config file or token is missing:", error);
-}
+const main = async () => {
+    try {
+        const { token } = await loadJson(configFilePath, new URL(import.meta.url));
+        if (!token || token.length === 0) {
+        }
+    }
+    catch (error) {
+        console.error("Failed to load the config file or token is missing:", error);
+    }
+    try {
+        const { exarotonApiKey } = await loadJson(configFilePath, new URL(import.meta.url));
+        console.log(exarotonApiKey);
+    }
+    catch (error) {
+        console.error("Failed to load the config file or exarotonApiKey is missing:", error);
+    }
+};
--- utilities/load-json.js
+++ utilities/load-json.js
@@ -8,8 +8,8 @@ import { fileURLToPath } from "url";
  * @param baseUrl - Usually `import.meta.url`
  */
 export default async function loadJson(configPath, baseUrl) {
-    const `${process.platform === 'win32' ? '' : '/'}${/file:\/{2,3}(.+)\/[^/]/.exec(import.meta.url)[1]}` = dirname(fileURLToPath(baseUrl));
-    const fullPath = resolve(`${process.platform === 'win32' ? '' : '/'}${/file:\/{2,3}(.+)\/[^/]/.exec(import.meta.url)[1]}`, configPath);
+    const dirname__ = dirname(fileURLToPath(baseUrl));
+    const fullPath = resolve(dirname__, configPath);
     const raw = await readFile(fullPath, "utf-8");
     return JSON.parse(raw);
 }
